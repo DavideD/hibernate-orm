1cd9d7190467a7ab6ecaecf2032513bfdce255ef
Davide D'Alto <davide@hibernate.org>
2019-11-27 17:09:38 +0000

    Hibernate Gradle plugin test

---
+++ hibernate-integrationtest-gradle-plugin/src/main/java/org/hibernate/orm/integrationtest/bytecode/model/Book.java
@@ -0,0 +1,94 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later
+ * See the lgpl.txt file in the root directory or http://www.gnu.org/licenses/lgpl-2.1.html
+ */
+package org.hibernate.orm.integrationtest.bytecode.model;
+
+import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.NaturalId;
+
+@Entity
+public class Book {
+
+	@Id
+	private Long id;
+
+	private String title;
+
+	@NaturalId
+	private String isbn;
+
+	@ManyToOne
+	private Person author;
+
+	public Book() {
+	}
+
+	public Book(Long id, String title, String isbn) {
+		this.id = id;
+		this.title = title;
+		this.isbn = isbn;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Person getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(Person author) {
+		this.author = author;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if ( this == o ) {
+			return true;
+		}
+		if ( o == null || getClass() != o.getClass() ) {
+			return false;
+		}
+		Book book = (Book) o;
+		return Objects.equals( title, book.title ) &&
+				Objects.equals( isbn, book.isbn );
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash( title, isbn );
+	}
+
+	@Override
+	public String toString() {
+		return title + ", '" + isbn;
+	}
+}
+
---
+++ hibernate-integrationtest-gradle-plugin/src/test/java/org/hibernate/jpa/test/packaging/BidirectionalOneToManyEnhancementsTest.java
@@ -0,0 +1,68 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.jpa.test.packaging;
+
+import org.hibernate.orm.integrationtest.bytecode.model.Book;
+import org.hibernate.orm.integrationtest.bytecode.model.Person;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Checks the association management enhancement is working
+ * when using the hibernate gradle plugin.
+ * TODO: Add tests to check that the classes have the additional fields and methods when the enhancement is active
+ */
+public class BidirectionalOneToManyEnhancementsTest {
+
+	final Book book = new Book( 5L, "Lost Connections: Why You’re Depressed and How to Find Hope", " 978-1408878729" );
+	final Person author = new Person( 5L, "Johann Hari" );
+
+	@Test
+	public void normalBehaviourWithoutEnhancements() throws Exception {
+		// This test should pass with or without enhancements (maybe?)
+		book.setAuthor( author );
+		author.getBooks().add( book );
+
+		assertEquals( 1, author.getBooks().size() );
+		assertEquals( book, author.getBooks().get( 0 ) );
+		assertEquals( author, book.getAuthor() );
+	}
+
+	@Test
+	public void addToField() throws Exception {
+		book.setAuthor( author );
+		// Normally, without enhancements, you would also need:
+		// author.getBooks().add( book );
+
+		assertEquals( 1, author.getBooks().size() );
+		assertEquals( book, author.getBooks().get( 0 ) );
+	}
+
+	@Test
+	public void addToCollection() throws Exception {
+		author.getBooks().add( book );
+		// Normally, without enhancements, you would also need:
+		// book.setAuthor( author );
+
+		assertEquals( author, book.getAuthor() );
+	}
+
+	@Test
+	public void setFieldToNull() throws Exception {
+		book.setAuthor( author );
+		author.getBooks().add( book );
+
+		book.setAuthor( null );
+		// Normally, without enhancements, you would also need:
+		// author.getBooks().remove( book );
+
+		assertTrue( author.getBooks().isEmpty() );
+	}
+}
